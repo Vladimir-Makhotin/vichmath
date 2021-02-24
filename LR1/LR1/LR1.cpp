31425a2d7b2f76d6926de3676a78ce8f77617d26
User <vladimir.makhotin@mail.ru>
2021-02-24 22:28:00 +0300

    commit on 24.02.2020

---
+++ LR1/LR1/LR1.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+bool del(double arr[3][4])
+{
+	double del = 0;
+	del = arr[0][0] * arr[1][1] * arr[2][2] + arr[0][1] * arr[1][2] * arr[2][0] + arr[1][0] * arr[2][1] * arr[0][2] - arr[0][2] * arr[1][1] * arr[2][0] - arr[0][1] * arr[1][0] * arr[2][2] - arr[0][0] * arr[1][2] * arr[2][1];
+	//cout << del << endl;
+	if (del == 0)
+		return false;
+	else
+		return true;
+}
+int main()
+{
+	//double mas[3][4] = { {2,-1,0,3},{0,5,2,7},{1,-1,3,4} }; 
+	double vec[3] = { 0,0,0 };
+	double mas[3][4]; 
+	cout << "Enter the member matrix" << endl;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 4; j++)
+			cin >> mas[i][j];
+	double t = mas[0][0]; 
+	if (del(mas) == true)
+	{
+		if (mas[0][0] != 1 && mas[0][0] != 0)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				mas[0][i] /= t;
+			}
+		}
+		if (mas[1][0]!=0)
+		{
+			t = mas[1][0];
+			for (int i = 0; i < 4; i++)
+			{
+				mas[1][i] /= t;
+				mas[1][i] -= mas[0][i];
+			}
+		}
+		if (mas[2][0] != 0)
+		{
+			t = mas[2][0];
+			for (int i = 0; i < 4; i++)
+			{
+				mas[2][i] /= t;
+				mas[2][i] -= mas[0][i];
+			}
+		}
+		if (mas[1][1] != 0)
+		{
+			t = mas[1][1];
+			for (int i = 0; i < 4; i++)
+			{
+				mas[1][i] /= t;
+			}
+		}
+		if (mas[2][1] != 0)
+		{
+			t = mas[2][1];
+			for (int i = 0; i < 4; i++)
+			{
+				mas[2][i] /= t;
+				mas[2][i] -= mas[1][i];
+			}
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				cout << mas[i][j] << '\t';
+			}
+			cout << endl;
+		}
+		vec[0] = mas[2][3] / mas[2][2];
+		vec[1] = (mas[1][3]-mas[1][2]*vec[0])/mas[1][1];
+		vec[2] = (mas[0][3] - mas[0][2] * vec[0] - mas[0][1] * vec[1]) / mas[0][0];
+		for (int i = 0; i < 3; i++)
+		{
+			cout << vec[i] << '\t' << endl;
+		}
+	}
+	else
+	{
+		cout << "System have not solution" << endl;
+	}
+
+
+}
---
+++ LR2/LR2/LR2.cpp
@@ -0,0 +1,204 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+bool diagonaldominance(double mas[3][4])
+{
+    int c = 0; double t=-1000;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i == 0&&j == 0)
+                if (abs(mas[i][j]) >= abs(mas[i][j + 1]) + abs(mas[i][j + 2]))
+                    c++;
+            if(i==1&&j==1)
+                if (abs(mas[i][j]) >= abs(mas[i][j + 1]) + abs(mas[i][j - 1]))
+                    c++;
+            if (i ==2&& j == 2)
+                if (abs(mas[i][j]) >= abs(mas[i][j - 1]) + abs(mas[i][j - 2]))
+                    c++;
+        }
+    }
+    if (c == 3)
+        return true;
+    else
+        return false;
+}
+void transform(double mas[3][4])
+{
+    double t = abs(mas[0][0]); int index[3] = { 0,0,0 }; double mas1[3][4] = { {0,0,0,0},{0,0,0,0},{0,0,0,0} }; double str[4] = {0,0,0,0};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (abs(mas[i][j]) >= t)
+            {
+                t = abs(mas[i][j]);
+                index[i] = j;
+            }
+        }
+
+        for (int j = 0; j < 4; j++)
+        {
+            if (mas1[index[i]][j] == 0)
+                mas1[index[i]][j] = mas[i][j];
+            else
+                str[j] = mas[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (mas1[i][j] == 0)
+                mas1[i][j] = str[j];
+        }
+    }
+    t = -1;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (i == 0 && j == 0)
+                    if (abs(mas1[i][j]) >= abs(mas1[i][j + 1]) + abs(mas1[i][j + 2]))
+                        ;
+                    else
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            if (abs(mas1[i][k]) > abs(t))
+                            {
+                                t = mas1[i][k];
+                            }
+                        }
+                        if (t < 0)
+                        {
+                            for (int k = 0; k < 4; k++)
+                                mas1[i][k] += abs(t);
+                        }
+                        if (t > 0)
+                        {
+                            for (int k = 0; k < 4; k++)
+                                mas1[i][k] -= t;
+                        }
+                    }
+                if (i == 1&&j == 1)
+                    if (abs(mas1[i][j]) >= abs(mas1[i][j + 1]) + abs(mas1[i][j - 1]))
+                        ;
+                    else
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            if (abs(mas1[i][k]) > abs(t))
+                            {
+                                t = mas1[i][k];
+                            }
+                        }
+                        if (t < 0)
+                        {
+                            for (int k = 0; k < 4; k++)
+                                mas1[i][k] += abs(t);
+                        }
+                        if (t > 0)
+                        {
+                            for (int k = 0; k < 4; k++)
+                                mas1[i][k] -= t;
+                        }
+                    }
+                if (i == 2&&j == 2)
+                    if (abs(mas1[i][j]) >= abs(mas1[i][j - 1]) + abs(mas1[i][j - 2]))
+                        ;
+                    else
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            if (abs(mas1[i][k]) > abs(t))
+                            {
+                                t = mas[i][k];
+                            }
+                        }
+                        if (t < 0)
+                        {
+                            for (int k = 0; k < 4; k++)
+                                mas1[i][k] += abs(t);
+                        }
+                        if (t > 0)
+                        {
+                            for (int k = 0; k < 4; k++)
+                                mas1[i][k] -= t;
+                        }
+                    }
+            }
+        }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            mas[i][j] = mas1[i][j];
+            //cout << mas1[i][j] << '\t';
+        }
+        cout << endl;
+    }
+}
+void solution(double array[3][4])
+{
+    double x10 = 0, x20 = 0, x30 = 0, dx1 = 1, dx2 = 1, dx3 = 1;
+    double t, x1, x2, x3, e = 0.001, vector[3] = { 0,0,0 }; int j=1;
+    while ((dx1 >= e) || (dx2 >= e) || (dx3 >= e))
+    {
+        cout << "Iteration number " << j << endl;
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == 0)
+            {
+                x10 = vector[i];
+                x1 = (array[i][3] - array[i][2] * vector[2] - array[i][1] * vector[1]) / array[i][0];
+                cout << "x" << i + 1 << "= " << x1 - remainder(x1, e) << endl;
+            }
+            if (i == 1)
+            {
+                x20 = vector[i];
+                x2 = (array[i][3] - array[i][2] * vector[2] - array[i][0] * vector[0]) / array[i][1];
+                cout << "x" << i + 1 << "= " << x2 - remainder(x2, e) << endl;
+            }
+            if (i == 2)
+            {
+                x30 = vector[i];
+                x3 = (array[i][3] - array[i][1] * vector[1] - array[i][0] * vector[0]) / array[i][2];
+                cout << "x" << i + 1 << "= " << x3 - remainder(x3, e) << endl;
+            }
+        }
+        vector[0] = x1;
+        vector[1] = x2;
+        vector[2] = x3;
+        dx1 = abs(vector[0] - x10);
+        dx2 = abs(vector[1] - x20);
+        dx3 = abs(vector[2] - x30);
+        j++;
+    }
+}
+int main()
+{
+    double mas[3][4];// = { {1.17, 4.19, -1.77, 4.25},{1.39, -1.45, -4.6, -1.55},{4.06, -5.42, 3.88, -6.01} };
+    double t;
+    cout << "Enter the matrix" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> mas[i][j];
+        }
+    }
+    if (diagonaldominance(mas) == true)
+    {
+        solution(mas);
+    }
+    else
+    {
+        transform(mas);
+        if (diagonaldominance(mas) == true)
+            solution(mas);
+        else
+            cout << "Matrix not solution" << endl;
+    }
+}
